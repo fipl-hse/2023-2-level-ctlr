7edef9bf7d9dd62d9f3e2442112d6127e730194e
AlisaKarpova <aliskakarpowa@yandex.ru>
2024-04-20 12:00:25 +0300

    some changes

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -13,9 +13,9 @@ from typing import Pattern, Union
 import requests
 from bs4 import BeautifulSoup
 
+from core_utils import constants
 from core_utils.article.article import Article
 from core_utils.article.io import to_meta, to_raw
-from core_utils import constants
 from core_utils.config_dto import ConfigDTO
 
 
