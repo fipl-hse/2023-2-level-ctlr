9c02ce4dc1211c0e15d15bed1d3feb6b7e1170ee
AlisaKarpova <aliskakarpowa@yandex.ru>
2024-04-24 12:19:07 +0300

    some changes

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -319,9 +319,9 @@ class HTMLParser:
         self.article.title = article_soup.find('h1').text
         date = article_soup.find("time").get('datetime')
         self.article.date = self.unify_date_format(str(date))
-        author = article_soup.find('p', class_="article-author")
-        if author:
-            self.article.author = [author.text.strip()]
+        author = article_soup.find(target="_blank").text
+        if author and author.strip():
+            self.article.author = [author]
         else:
             self.article.author = ["NOT FOUND"]
         topics = article_soup.find_all(class_="article-tag-link")
