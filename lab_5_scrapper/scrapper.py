f4baf7beaac1f969e31a5d2eb5a6c8c118cf6182
Arina Savina <arinasavina330@gmail.com>
2024-04-25 07:22:27 +0300

    Scrapper, Arina Savina - 22FPL1

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -374,9 +374,9 @@ def main() -> None:
     for i, full_url in enumerate(crawler.urls, 1):
         parser = HTMLParser(full_url=full_url, article_id=i, config=configuration)
         article = parser.parse()
-        # if isinstance(article, Article):
-        to_raw(article)
-        to_meta(article)
+        if isinstance(article, Article):
+            to_raw(article)
+            to_meta(article)
 
 
 if __name__ == "__main__":
