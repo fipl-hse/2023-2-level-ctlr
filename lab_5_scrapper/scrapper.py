dcbe2a307d14f15159e4029bbaa873773910ec06
Kate Nushkina <nushkina03@mail.ru>
2024-04-26 21:35:46 +0300

    fix imports order

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -18,6 +18,7 @@ from core_utils.article.article import Article
 from core_utils.article.io import to_meta, to_raw
 from core_utils.config_dto import ConfigDTO
 
+
 class IncorrectSeedURLError(Exception):
     pass
 
