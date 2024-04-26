83f670a1f75d2472b59417edb5b0501f70b7c8e1
Dzianna7 <Dzianna7@mail.ru>
2024-04-27 00:04:47 +0300

    corrections

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -17,7 +17,6 @@ from core_utils import constants
 from core_utils.article.article import Article
 from core_utils.article.io import to_meta, to_raw
 from core_utils.config_dto import ConfigDTO
-from core_utils.constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
 
 
 class IncorrectSeedURLError(Exception):
