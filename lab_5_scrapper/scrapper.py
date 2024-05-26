b127d8fcc2caeb38c74caa9b138decb75e47e4e9
elena <eiermal@edu.hse.ru>
2024-05-26 09:37:20 +0300

    ,,

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -77,15 +77,16 @@ class Config:
 
         self.path_to_config = path_to_config
         self._validate_config_content()
+
         self.config = self._extract_config_content()
 
-        self._encoding = self.config.encoding
-        self._headers = self.config.headers
-        self._headless_mode = self.config.headless_mode
-        self._num_articles = self.config.total_articles
         self._seed_urls = self.config.seed_urls
-        self._should_verify_certificate = self.config.should_verify_certificate
+        self._num_articles = self.config.total_articles
+        self._headers = self.config.headers
+        self._encoding = self.config.encoding
         self._timeout = self.config.timeout
+        self._should_verify_certificate = self.config.should_verify_certificate
+        self._headless_mode = self.config.headless_mode
 
     def _extract_config_content(self) -> ConfigDTO:
         """
@@ -95,18 +96,9 @@ class Config:
             ConfigDTO: Config values
         """
 
-        with open(self.path_to_config, "r", encoding="utf-8") as config_file:
-            config = json.load(config_file)
-
-        return ConfigDTO(
-             seed_urls=config['seed_urls'],
-             total_articles_to_find_and_parse=config['total_articles_to_find_and_parse'],
-             headers=config['headers'],
-             encoding=config['encoding'],
-             timeout=config['timeout'],
-             should_verify_certificate=config['should_verify_certificate'],
-             headless_mode=config['headless_mode']
-          )
+        with open(file=self.path_to_config, mode='r', encoding='utf-8') as file:
+            config_content = json.load(file)
+        return ConfigDTO(**config_content)
 
     def _validate_config_content(self) -> None:
         """
@@ -114,14 +106,17 @@ class Config:
         """
         config = self._extract_config_content()
 
-        if not (isinstance(config.seed_urls, list)
-                and all(re.match(r'https?://(www.)?', seed_url) for seed_url in config.seed_urls)):
+        if not (
+                isinstance(config.seed_urls, list)
+                and all(re.match(r'https://www\.vokrugsveta\.ru/news/?(page-)?\d/*', url)
+                        for url in config.seed_urls)
+        ):
             raise IncorrectSeedURLError
 
-        if not isinstance(config.total_articles, int) or not config.total_articles > 0:
+        if not isinstance(config.total_articles, int) or config.total_articles <= 0:
             raise IncorrectNumberOfArticlesError
 
-        if not 0 < config.total_articles < 150:
+        if config.total_articles < 1 or config.total_articles > 150:
             raise NumberOfArticlesOutOfRangeError
 
         if not isinstance(config.headers, dict):
@@ -130,13 +125,11 @@ class Config:
         if not isinstance(config.encoding, str):
             raise IncorrectEncodingError
 
-        if not isinstance(config.timeout, int) or not 0 <= config.timeout < 60:
+        if not isinstance(config.timeout, int) or config.timeout < 0 or config.timeout > 60:
             raise IncorrectTimeoutError
 
-        if not isinstance(config.should_verify_certificate, bool):
-            raise IncorrectVerifyError
-
-        if not isinstance(config.headless_mode, bool):
+        if (not isinstance(config.should_verify_certificate, bool)
+                or not isinstance(config.headless_mode, bool)):
             raise IncorrectVerifyError
 
     def get_seed_urls(self) -> list[str]:
@@ -216,12 +209,10 @@ def make_request(url: str, config: Config) -> requests.models.Response:
     """
     sleep(randrange(3))
 
-    return requests.get(
-        url=url,
-        headers=config.get_headers(),
-        timeout=config.get_timeout(),
-        verify=config.get_verify_certificate()
-    )
+    headers = config.get_headers()
+    timeout = config.get_timeout()
+
+    return requests.get(url=url, headers=headers, timeout=timeout)
 
 
 class Crawler:
@@ -271,13 +262,13 @@ class Crawler:
             if not response.ok:
                 continue
 
-            article_soup = BeautifulSoup(response.text, features='lxml')
-            new_url = self._extract_url(article_soup)
+            new_url = seed_url
             while new_url:
-                if len(self.urls) == self.config.get_num_articles():
-                    break
-                self.urls.append(new_url)
+                article_soup = BeautifulSoup(response.text, features='lxml')
                 new_url = self._extract_url(article_soup)
+
+                if new_url:
+                    self.urls.append(new_url)
 
             if len(self.urls) == self.config.get_num_articles():
                 break
