ac873e998a70ec4ae4d76daac3e8cb39177f5f5d
Andrey Gusev <andrey.g9.ag@gmail.com>
2024-05-26 20:27:54 +0300

    added function call to main

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -318,7 +318,8 @@ def main() -> None:
     Entrypoint for pipeline module.
     """
     corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
-    pipeline = TextProcessingPipeline(corpus_manager)
+    pipeline = TextProcessingPipeline(corpus_manager, UDPipeAnalyzer())
+    pipeline.run()
 
 
 if __name__ == "__main__":
