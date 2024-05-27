ac2d4b9dabfc6982ac3a9abcfea95dd63d207945
Соня <saonofrienko@edu.hse.ru>
2024-05-27 03:01:55 +0300

    htmlparser - init

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -278,6 +278,12 @@ class HTMLParser:
             config (Config): Configuration
         """
 
+        self.full_url = full_url
+        self.article_id = article_id
+        self.config = config
+        self.article = Article(self.full_url, self.article_id)
+
+
     def _fill_article_with_text(self, article_soup: BeautifulSoup) -> None:
         """
         Find text of article.
