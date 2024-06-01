f163df68a9008ee1d969cf253d3a2285e47c4370
RostislavHmelevski <warian11031978@gmail.com>
2024-06-01 17:36:00 +0300

    some steps forward

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -2,16 +2,61 @@
 Crawler implementation.
 """
 # pylint: disable=too-many-arguments, too-many-instance-attributes, unused-import, undefined-variable
-import pathlib
-import json
-import shutil
 import datetime
+import json
+import pathlib
 import re
+import shutil
 from typing import Pattern, Union
+
 import requests
 from bs4 import BeautifulSoup
+
+from core_utils.article.article import Article
+from core_utils.article.io import to_meta, to_raw
 from core_utils.config_dto import ConfigDTO
 from core_utils.constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
+
+class IncorrectSeedURLError(Exception):
+    """
+    Seed URL does not match standard pattern.
+    """
+
+
+class IncorrectNumberOfArticlesError(Exception):
+    """
+    Total number of articles to parse is not integer.
+    """
+
+
+class NumberOfArticlesOutOfRangeError(Exception):
+    """
+    Total number of articles is out of range from 1 to 150.
+    """
+
+
+class IncorrectHeadersError(Exception):
+    """
+    Headers are not in a form of dictionary.
+    """
+
+
+class IncorrectEncodingError(Exception):
+    """
+    encoding must be specified as a string.
+    """
+
+
+class IncorrectTimeoutError(Exception):
+    """
+    timeout value must be a positive integer less than 60.
+    """
+
+
+class IncorrectVerifyError(Exception):
+    """
+    verify certificate value must either be True or False.
+    """
 
 class Config:
     """
@@ -26,8 +71,15 @@ class Config:
             path_to_config (pathlib.Path): Path to configuration.
         """
         self.path_to_config = path_to_config
+        self._config = self._extract_config_content()
         self._validate_config_content()
-        self.config_dto = self._extract_config_content()
+        self._seed_urls = self._config.seed_urls
+        self._num_articles = self._config.total_articles
+        self._headers = self._config.headers
+        self._encoding = self._config.encoding
+        self._timeout = self._config.timeout
+        self._should_verify_certificate = self._config.should_verify_certificate
+        self._headless_mode = self._config.headless_mode
 
     def _extract_config_content(self) -> ConfigDTO:
         """
@@ -36,38 +88,34 @@ class Config:
         Returns:
             ConfigDTO: Config values
         """
-        with open(self.path_to_config, 'r', encoding = 'utf-8') as f:
-            config = json.load(f)
-        return ConfigDTO
+        with open(file=self.path_to_config, mode='r', encoding='utf-8') as file:
+            config = json.load(file)
+        return ConfigDTO(**config)
 
     def _validate_config_content(self) -> None:
         """
         Ensure configuration parameters are not corrupt.
         """
-        valid_url = self.config_dto.seed_urls
-        if not valid_url.startswith("https?://(www.)?") and not isinstance(valid_url, str):
-            raise IncorrectSeedURLError
+        for seed_url in self.config.seed_urls:
+            if not (isinstance(self.config.seed_urls, list) and all (re.match(r"https?://(www.)?", seed_url))):
+                raise IncorrectSeedURLError
 
-        number_articles = self.config_dto.total_articles
-        if number_articles > 150 or number_articles < 1:
+        if self._num_articles > 150 or self._num_articles < 1:
             raise NumberOfArticlesOutOfRangeError
-        if number_articles <= 0 or not isinstance(number_articles, int):
+
+        if self._config.total_articles <= 0 or not isinstance(self._config.total_articles, int):
             raise IncorrectNumberOfArticlesError
 
-        headers_articles = self.config_dto.headers
-        if not isinstance(headers_articles, dict):
+        if not isinstance(self._config.headers, dict):
             raise IncorrectHeadersError
-        encode_articles = self.config_dto.encoding
-        if not isinstance(encode_articles, str):
+
+        if not isinstance(self._config.encoding, str):
             raise IncorrectEncodingError
 
-        timeout_articles = self.config_dto.timeout
-        if timeout_articles > 60 or timeout_articles < 0:
+        if self._config.timeout > 60 or self._config.timeout < 0:
             raise IncorrectTimeoutError
 
-        certificate_articles = self.config_dto.should_verify_certificate
-        headless_articles = self.config_dto.headless_mode
-        if not isinstance(certificate_articles, bool) or not isinstance (headless_articles, bool):
+        if not isinstance(self._config.should_verify_certificate, bool) or not isinstance (self._config.headless_mode, bool):
             raise IncorrectVerifyError
 
     def get_seed_urls(self) -> list[str]:
@@ -77,7 +125,8 @@ class Config:
         Returns:
             list[str]: Seed urls
         """
-        return self._seed_urls()
+        return self._seed_urls
+
 
     def get_num_articles(self) -> int:
         """
@@ -122,7 +171,7 @@ class Config:
         Returns:
             bool: Whether to verify certificate or not
         """
-        return self._verify_certificate
+        return self._should_verify_certificate
 
     def get_headless_mode(self) -> bool:
         """
@@ -162,6 +211,7 @@ class Crawler:
         """
         self.config = config
         self.urls = []
+        self.url_pattern = 'https://donday.ru/lenta'
 
     def _extract_url(self, article_bs: BeautifulSoup) -> str:
         """
@@ -173,24 +223,26 @@ class Crawler:
         Returns:
             str: Url from HTML
         """
-        all_links = article_bs.find_all('a', #???)
-        for link in all_links:
-            url = link['href']
-        ###i dont know what to do here :(
+        url = ""
+        all_news = article_bs.find_all('h3')
+        for news in all_news:
+            url = news.find('a')['href']
+            if url not in self.urls:
+                break
+        return url
+
 
     def find_articles(self) -> None:
         """
         Find articles.
         """
 
-        while len(self_url) < self.config.get_num_articles():
+        while len(self.get_search_urls()) < self.config.get_num_articles():
             for seed_url in self.get_search_urls():
                 response = make_request(seed_url, self.config)
                 if not response.ok:
                     continue
-                soup = BeautifulSoup(response.text, 'html.parser')
-                extra_urls = self._extract_url(soup)
-                self.urls.append(extra_urls)
+                self.urls.append(self._extract_url(BeautifulSoup(response.text, "lxml")))
 
     def get_search_urls(self) -> list:
         """
@@ -219,7 +271,11 @@ class HTMLParser:
             article_id (int): Article id
             config (Config): Configuration
         """
-
+        self.full_url = full_url
+        self.article_id = article_id
+        self.config = config
+        self.article = Article(self.full_url, self.article_id)
+ 
     def _fill_article_with_text(self, article_soup: BeautifulSoup) -> None:
         """
         Find text of article.
@@ -227,6 +283,11 @@ class HTMLParser:
         Args:
             article_soup (bs4.BeautifulSoup): BeautifulSoup instance
         """
+        texts = []
+        allNews = article_soup.findAll(itemprop="articleBody")
+        for paragraph in allNews:
+            texts.append(paragraph.text)
+            self.article.text = ''.join(texts)
 
     def _fill_article_with_meta_information(self, article_soup: BeautifulSoup) -> None:
         """
@@ -254,7 +315,15 @@ class HTMLParser:
         Returns:
             Union[Article, bool, list]: Article instance
         """
+        response = make_request(url=self.full_url, config=self.config)
+        if not response.ok:
+            return False
+        article_bs = BeautifulSoup(response.text, features="lxml")
 
+        self._fill_article_with_text(article_bs)
+        self._fill_article_with_meta_information(article_bs)
+
+        return self.article
 
 def prepare_environment(base_path: Union[pathlib.Path, str]) -> None:
     """
