84f1138045a25c51103d6da4c3d55bcb4379f098
alesiamashkovtseva <alesiamashkovtseva@gmail.com>
2024-06-02 03:53:13 +0300

    fixing

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -64,8 +64,9 @@ class CorpusManager:
         if not self.path_to_raw_txt_data.is_dir():
             raise NotADirectoryError
 
-        if not any(self.path_to_raw_txt_data.iterdir()):
-            raise EmptyDirectoryError
+        for child in self.path_to_raw_txt_data.iterdir():
+            if not any(child.iterdir()):
+                raise EmptyDirectoryError
 
         meta_files = list(self.path_to_raw_txt_data.glob('*_meta.json'))
         raw_files = list(self.path_to_raw_txt_data.glob('*_raw.txt'))
