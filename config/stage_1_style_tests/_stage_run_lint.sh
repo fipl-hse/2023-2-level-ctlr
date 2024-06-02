06f1aef8180a75c9bf88addb531d927851b424fa
NovIrina <zahara.novikova2014@yandex.ru>
2024-06-02 23:46:00 +0300

    rollback

--- config/stage_1_style_tests/_stage_run_lint.sh
+++ config/stage_1_style_tests/_stage_run_lint.sh
@@ -39,7 +39,6 @@ for LAB_NAME in $LABS; do
 
   if [[ ${LAB_NAME} == 'lab_6_pipeline' ]]; then
     export PYTHONPATH=${PYTHONPATH}:lab_6_pipeline/universal_dependencies
-    cat lab_6_pipeline/pipeline.py
   fi
 
   IGNORE_OPTION=""
